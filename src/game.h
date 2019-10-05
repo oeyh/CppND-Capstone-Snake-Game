7d02b6e037696aff17b2b3237fea18f92153cea2
oeyh <8153134+oeyh@users.noreply.github.com>
2019-10-05 14:28:04 -0700

    Add Level inner class and write parser to read level config files.

--- src/game.h
+++ src/game.h
@@ -6,6 +6,8 @@
 #include "controller.h"
 #include "renderer.h"
 #include "snake.h"
+#include <fstream>
+#include <sstream>
 
 class Game {
  public:
@@ -17,7 +19,81 @@ class Game {
   
 
  private:
-  
+  // Level stores level information
+  struct Level {
+    int level_id;
+    int food_cnt;
+    int stone_cnt;
+    std::vector<SDL_Point> food;
+    std::vector<SDL_Point> stone;
+    // snake speed, etc.
+
+    // level file format
+    // level id
+    // food count, then food coordinates
+    // stone count, then stone coordinates
+    Level(std::string levelfile) {
+      std::ifstream infile("levels/" + levelfile);
+      // check if open is success
+      if (!infile) {
+        std::cout << "Cannot open file.\n";
+        exit(1);
+      }
+
+      // parse level file
+      std::string line;
+      // first line - level id
+      std::getline(infile, line);
+      level_id = std::stoi(line);
+
+      // food count
+      std::getline(infile, line);
+      food_cnt = std::stoi(line);
+
+      // food points
+      std::getline(infile, line);
+      // split line at whitespaces
+      std::istringstream iss(line);
+      std::istream_iterator<std::string> start(iss), end;
+      std::vector<std::string> results(start, end);
+      for (std::string result : results) {
+        std::string temp = result.substr(1, result.length() - 2); // example: "7,23"
+        std::istringstream ss(temp);
+        std::string num_str;
+        std::getline(ss, num_str, ','); // get first number
+        SDL_Point point;
+        point.x = stoi(num_str);
+        std::getline(ss, num_str);    // get second number
+        point.y = stoi(num_str);
+
+        food.push_back(point);
+      }
+
+      // stone count
+      std::getline(infile, line);
+      stone_cnt = std::stoi(line);
+
+      // stone points
+      std::getline(infile, line);
+      std::istringstream iss2(line);
+      std::istream_iterator<std::string> start2(iss2), end2;
+      std::vector<std::string> results2(start2, end2);
+      for (std::string result : results2) {
+        std::string temp = result.substr(1, result.length() - 2); // example: "7,23"
+        std::istringstream ss(temp);
+        std::string num_str;
+        std::getline(ss, num_str, ','); // get first number
+        SDL_Point point;
+        point.x = stoi(num_str);
+        std::getline(ss, num_str);    // get second number
+        point.y = stoi(num_str);
+
+        stone.push_back(point);
+      }
+
+      infile.close();
+    }
+  };
 
   Snake snake;
   std::vector<SDL_Point> food;
